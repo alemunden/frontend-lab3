651b4cfa64671c8cc57f7a0f527364bea12cebd8
ale munden <amunden@my.bcit.ca>
2024-09-29 20:03:46 -0700

    initial commit

---
+++ .history/src/App_20240928171805.jsx
@@ -0,0 +1,92 @@
+import "./App.css";
+import news_feed from './sample_news_stories.json';
+
+function random(n) {
+  return Math.floor(Math.random() * n);
+}
+
+function App() {
+
+  function handleInnerClick(e) {
+    const element = e.target;
+    const red = random(255);
+    const green = random(255);
+    const blue = random(255);
+
+    element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
+
+  function handleOuterClick(e) {
+    const element = e.currentTarget;
+    const color = random(255);
+
+    setTimeout(() => {
+      element.style.backgroundColor = `rgb(${color}, ${color}, ${color})`;
+    }, 500);
+  }
+
+  return (
+    <>
+      <div className="outer" onClick={handleOuterClick}>
+        <div className="inner" onClick={handleInnerClick}></div>
+      </div>
+
+      <div className="container">
+        <Story/>
+      </div>
+    </>
+      
+  );
+}
+/*
+function Tile(props) {
+  const background = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+
+  function handleDelete(e) {
+    if ( e.target === e.currentTarget) {
+      // you clicked on the tile itself
+      return;
+    }
+
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    e.currentTarget.remove(); // otherwise, remove the tile
+  }
+
+  return (
+    <div className="tile" style={{ backgroundColor: background }} onClick={handleDelete}>
+      <p>{props.name}</p>
+      <span>x</span>
+    </div>
+  );
+}
+*/
+
+function Story(props) {
+  const stories = news_feed.results;
+  const background = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  function deleteButton(e) {
+    if(e.target === e.currentTarget) {
+      return;
+    }
+
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    e.currentTarget.remove();
+  }
+
+  return(
+    <div className="tile" style={{ backgroundColor: background }} onClick={deleteButton}>
+      <div>
+        <p>{stories.title}</p>
+      </div>
+      <span>x</span>
+    </div>
+  )
+}
+
+export default App;
